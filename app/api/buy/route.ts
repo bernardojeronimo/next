4411343b5b6b980afd6669ab0ddc9709fe9aecf4
Bernardo Jerónimo <bernardoantjeronimo@gmail.com>
2024-12-30 19:44:55 +0000

    Corrigi o erro da route do buy

--- app/api/buy/route.ts
+++ app/api/buy/route.ts
@@ -1,5 +1,5 @@
-export async function POST(data: any) {
-  const body = await data.json();
+export async function POST(request: Request) {
+  const body = await request.json();
   const resp = await fetch('https://deisishop.pythonanywhere.com/buy/', {
     method: 'POST',
     body: JSON.stringify(body),
@@ -7,9 +7,9 @@ export async function POST(data: any) {
   if (!resp.ok) {
     return Response.json({ error: resp.statusText }, { status: resp.status })
   }
-  const data2 = await resp.json();
-  if (data2.hasOwnProperty('error')) {
-    return Response.json({ error: data2.error }, { status: 500 })
+  const data = await resp.json();
+  if (data.hasOwnProperty('error')) {
+    return Response.json({ error: data.error }, { status: 500 })
   }
-  return Response.json(data2)
+  return Response.json(data)
 }
