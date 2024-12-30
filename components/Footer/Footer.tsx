5cc5c1b5c4a207c2d66fbc275bb709909a731d2f
Bernardo Jerónimo <bernardoantjeronimo@gmail.com>
2024-12-30 19:54:48 +0000

    footer atualizado

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Bernardo Jerónimo | 2024</p>
+      <p>2024 © Bernardo Jerónimo</p>
     </footer>
   );
 }
